f2bfcba85cf76445d4db68857ede82f9e3e2e59e
Beka Lomsadze <lomsadze.beka@gmail.com>
2025-01-30 23:40:27 +0400

    created loading component

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import { AssetsTable } from "@/components/dashboard";
+import { PageTitle } from "@/components/common";
+import { fetchAssets } from "./actions/assets.action";
+
+const AssetsPage = async () => {
+  const assetsData = await fetchAssets();
+
+  return (
+    <>
+      <PageTitle title="Prices" />
+      <AssetsTable assetsData={assetsData?.data || []} />
+    </>
+  );
+};
+
+export default AssetsPage;
